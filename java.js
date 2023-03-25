93a4bc96edec93bd114d1baadf3d39ae34f51136
torstenbergersen <52758041+torstenbergersen@users.noreply.github.com>
2023-03-25 18:05:47 -0400

    Finish basic console.log game

--- java.js
+++ java.js
@@ -1,6 +1,56 @@
-// create function called getComputerChoice
-// function should return either "rock" , "paper" or "scissors"
-let arrayOfChoices = ["rock", "paper", "scissors"]
-let getComputerChoice = arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)];
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+      return "It's a tie!";
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+               playerSelection === 'paper' && computerSelection === 'rock' ||
+               playerSelection === 'scissors' && computerSelection === 'paper') {
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+      return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+  }
+  
+  function computerPlay() {
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * choices.length)];
+  }
+  
+  async function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let i = 1; i <= 5; i++) {
+      let playerSelection = prompt("Enter your choice (rock, paper, or scissors): ").toLowerCase();
 
-console.log();
+      while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
+        console.log("Invalid choice. Please enter rock, paper, or scissors.");
+        playerSelection = prompt("Enter your choice (rock, paper, or scissors): ").toLowerCase();
+      }
+      const computerSelection = computerPlay();
+  
+      console.log(`Round ${i}:`);
+      console.log(`You chose ${playerSelection}`);
+      console.log(`Computer chose ${computerSelection}`);
+  
+      const result = playRound(playerSelection, computerSelection);
+      console.log(result);
+  
+      if (result.includes("win")) {
+        playerScore++;
+      } else if (result.includes("lose")) {
+        computerScore++;
+      }
+      await new Promise(resolve => setTimeout(resolve, 3000));
+    }
+  
+    console.log(`Final score: You ${playerScore}, Computer ${computerScore}`);
+    if (playerScore > computerScore) {
+      console.log("You win the game!");
+    } else if (computerScore > playerScore) {
+      console.log("You lose the game!");
+    } else {
+      console.log("It's a tie game!");
+    }
+  }
+  
+  game();
