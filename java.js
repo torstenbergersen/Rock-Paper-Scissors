67e55dd4989869cbdc2e5ba8a0534283102e8f96
torstenbergersen <52758041+torstenbergersen@users.noreply.github.com>
2023-03-25 19:43:52 -0400

    Clean up code. Shorten to 80 characters

--- java.js
+++ java.js
@@ -8,49 +8,52 @@ function playRound(playerSelection, computerSelection) {
     } else {
       return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
-  }
+}
   
-  function computerPlay() {
-    const choices = ['rock', 'paper', 'scissors'];
-    return choices[Math.floor(Math.random() * choices.length)];
-  }
+function computerPlay() {
+  const choices = ['rock', 'paper', 'scissors'];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
   
-  async function game() {
-    let playerScore = 0;
-    let computerScore = 0;
+async function game() {
+  let playerScore = 0;
+  let computerScore = 0;
   
-    for (let i = 1; i <= 5; i++) {
-      let playerSelection = prompt("Enter your choice (rock, paper, or scissors): ").toLowerCase();
+for (let i = 1; i <= 5; i++) {
+  let playerSelection = prompt("Enter your choice (rock, paper, or scissors): ").
+  toLowerCase();
 
-      while (playerSelection !== "rock" && playerSelection !== "paper" && playerSelection !== "scissors") {
-        console.log("Invalid choice. Please enter rock, paper, or scissors.");
-        playerSelection = prompt("Enter your choice (rock, paper, or scissors): ").toLowerCase();
-      }
-      const computerSelection = computerPlay();
+  while (playerSelection !== "rock" && playerSelection !== "paper" && 
+  playerSelection !== "scissors") {
+    console.log("Invalid choice. Please enter rock, paper, or scissors.");
+    playerSelection = prompt("Enter your choice (rock, paper, or scissors): ").
+    toLowerCase();
+  }
+  const computerSelection = computerPlay();
   
-      console.log(`Round ${i}:`);
-      console.log(`You chose ${playerSelection}`);
-      console.log(`Computer chose ${computerSelection}`);
+  console.log(`Round ${i}:`);
+  console.log(`You chose ${playerSelection}`);
+  console.log(`Computer chose ${computerSelection}`);
   
-      const result = playRound(playerSelection, computerSelection);
-      console.log(result);
+  const result = playRound(playerSelection, computerSelection);
+  console.log(result);
   
-      if (result.includes("win")) {
-        playerScore++;
-      } else if (result.includes("lose")) {
-        computerScore++;
-      }
-      await new Promise(resolve => setTimeout(resolve, 3000));
-    }
-  
-    console.log(`Final score: You ${playerScore}, Computer ${computerScore}`);
-    if (playerScore > computerScore) {
-      console.log("You win the game!");
-    } else if (computerScore > playerScore) {
-      console.log("You lose the game!");
-    } else {
-      console.log("It's a tie game!");
-    }
+  if (result.includes("win")) {
+    playerScore++;
+  } else if (result.includes("lose")) {
+    computerScore++;
+  }
+  await new Promise(resolve => setTimeout(resolve, 3000));
   }
   
-  game();
+  console.log(`Final score: You ${playerScore}, Computer ${computerScore}`);
+  if (playerScore > computerScore) {
+    console.log("You win the game!");
+  } else if (computerScore > playerScore) {
+    console.log("You lose the game!");
+  } else {
+    console.log("It's a tie game!");
+  }
+}
+  
+game();
