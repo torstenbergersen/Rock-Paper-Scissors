38b23e590f4ee4c8a04fa9e83e4ccc536b3146f0
torstenbergersen <52758041+torstenbergersen@users.noreply.github.com>
2023-03-25 13:04:07 -0400

    Begin game planning

---
+++ java.js
@@ -0,0 +1,6 @@
+// create function called getComputerChoice
+// function should return either "rock" , "paper" or "scissors"
+let arrayOfChoices = ["rock", "paper", "scissors"]
+let getComputerChoice = arrayOfChoices[Math.floor(Math.random() * arrayOfChoices.length)];
+
+console.log();
